c36a2ea5016a89b1bf02ce9c70771dfece6a7ff1
Mark Bounthavong <mbounthavong@gmail.com>
2024-12-20 08:37:02 -0800

    Create R training - part 2 (two group comparisons).R

---
+++ R codes/R training - part 2 (two group comparisons).R
@@ -0,0 +1,197 @@
+################################################################################
+# Title:        R tutorial - Part 2 -- Bivariate comparisons
+# By:           Mark Bounthavong
+# Output:       R 
+# R Version:    4.2.3 "Shortstop Beagle"
+# Category:     Biostatistics
+# Date:         07 August 2021
+# Updated:      20 December 2024
+# Updated by:   Mark Bounthavong
+################################################################################
+
+# This tutorial will introduce students to using R and R studio.
+
+
+## Clear environment
+rm(list = ls())
+
+
+###############################
+## Libraries
+###############################
+if (!require("pacman")) install.packages("pacman"); library("pacman")
+p_load("ggplot2", 
+       "psych", 
+       "tidyverse")
+
+
+###############################
+## Import file from GitHub site
+###############################
+urlfile = "https://raw.githubusercontent.com/mbounthavong/VA-Resident-Study-Design-and-Biostats-Review/refs/heads/main/Data/diabetes.csv"
+diabetes.data <- read.csv(url(urlfile))
+head(diabetes.data)
+
+###############################
+## Data manipulation
+###############################
+## Add subject id
+diabetes.data["subject_id"] <- seq(1, 768)
+
+## Rearrange: subject_id first
+diabetes.data <- diabetes.data %>% select(subject_id, everything())  
+
+
+
+
+###############################
+## Creating new variables
+###############################
+
+#### Method 1: Create a group -- High pregnancy (3.5 or more pregnancies) and Low pregnancy groups (< 3.5 pregnancies)
+diabetes.data$High.Pregnancy[diabetes.data$Pregnancies <=3.5] = 0
+diabetes.data$High.Pregnancy[diabetes.data$Pregnancies > 3.5] = 1
+diabetes.data$High.Pregnancy
+
+#### Method 2: Create a group -- High pregnancy (3.5 or more pregnancies) and Low pregnancy groups (< 3.5 pregnancies)
+diabetes.data$High.Pregnancy <- ifelse(diabetes.data$Pregnancies > 3.5, c("High-preg"), c("Low-preg"))
+diabetes.data$High.Pregnancy
+
+#### Look at the histogram distributions between High and Low pregnancies
+par(mfrow = c(1, 2))  ### Build the matrix template
+hist(diabetes.data$Pregnancies[diabetes.data$High.Pregnancy=="Low-preg"])   ## hist for Low-preg group
+hist(diabetes.data$Pregnancies[diabetes.data$High.Pregnancy=="High-preg"])  ## hist for High-preg group
+
+#### Example 1: Create a variable called High thickness (defined as >= 25 mm):
+diabetes.data$High.thickness <- ifelse(diabetes.data$SkinThickness >= 25, c("High thickness"), c("Low thickness"))
+diabetes.data$High.thickness
+
+par(mfrow = c(1, 2))
+hist(diabetes.data$SkinThickness[diabetes.data$High.thickness=="High thickness"])
+hist(diabetes.data$SkinThickness[diabetes.data$High.thickness=="Low thickness"])
+
+
+
+###############################
+## Descriptive analysis
+###############################
+
+
+### By Groups
+### High.Pregnancy = 0 (<= 3.5 pregnancies)
+### High.Pregnancy = 1 (> 3.5 pregnancies)
+describeBy(diabetes.data$BMI, group = diabetes.data$High.Pregnancy)
+
+### For all variables
+describeBy(diabetes.data, group = diabetes.data$High.Pregnancy)
+
+
+
+
+###############################
+## Statistical tests
+###############################
+
+#### View distributions
+hist(diabetes.data$Pregnancies)
+
+#### Method #1
+par(mfrow = c(1, 2))
+hist(diabetes.data$Pregnancies[diabetes.data$Outcome==0])
+hist(diabetes.data$Pregnancies[diabetes.data$Outcome==1])
+
+#### Method #2
+ggplot(diabetes.data, aes(x = Pregnancies)) +
+  geom_histogram(fill = "white", colour = "black") +
+  facet_grid(Outcome ~ .)
+
+
+
+#### One-sample t-test
+# H0: The mean number of pregnancies is not different from 3.5 (H0: mu = 3.5)
+# Ha: The mean number of pregnancies is different from 3.5 (Ha: mu != 3.5)
+t.test(diabetes.data$Pregnancies, mu = 3.5) 
+
+
+#### Parametric two-group comparison - Independent t test
+# H0: The mean number of pregnancies in the group with the outcome is not 
+#     different from the mean number of pregnancies in the group with 
+#     no Outcomes (H0: mu1 = mu2)
+# Ha: The mean number of pregnancies in the group with the outcome is 
+#     different from the mean number of pregnancies in the group with 
+#     no Outcomes (Ha: mu1 != mu2)
+t.test(diabetes.data$Pregnancies ~ diabetes.data$Outcome, alternative = "two.sided", mu = 0, paired = FALSE, var.equal = FALSE, conf.level = 0.95)
+t.test(diabetes.data$Pregnancies ~ diabetes.data$Outcome)
+
+attach(diabetes.data)          ### You can attached the data 
+t.test(Outcome, Pregnancies)   ### No need to preface each var with data set
+detach(diabetes.data)
+
+#### Non-parametric two-group comparison - Mann-Whitney U test
+wilcox.test(diabetes.data$Outcome, diabetes.data$Pregnancies)
+
+
+#### Test for normality (Kolmogorov-Smirnoff test)
+## HO: There is no difference in the distribution of the variable from a normal distribution
+## Ha: There is a difference in the distribution of the variable from a normal distribution
+ks.test(diabetes.data$Pregnancies, pnorm)
+hist(diabetes.data$Pregnancies)
+
+#### Test for normality (Shapiro-Wilk's test)
+shapiro.test(diabetes.data$Pregnancies)
+shapiro.test(diabetes.data$Pregnancies[diabetes.data$Outcome==0])
+shapiro.test(diabetes.data$Pregnancies[diabetes.data$Outcome==1])
+
+#### Example 1: Is glucose normally distributed?
+hist(diabetes.data$Glucose)
+ks.test(diabetes.data$Glucose, pnorm)
+shapiro.test(diabetes.data$Glucose)
+
+#### Example 2: Is BP normally distributed?
+hist(diabetes.data$BloodPressure)
+ks.test(diabetes.data$BloodPressure, pnorm)
+shapiro.test(diabetes.data$BloodPressure)
+
+#### EXample 3: Is BMI normally distributed?
+hist(diabetes.data$BMI)
+ks.test(diabetes.data$BMI, pnorm)
+shapiro.test(diabetes.data$BMI)
+
+#### Example 4: Is skin thickness normally distributed?
+hist(diabetes.data$SkinThickness)
+ks.test(diabetes.data$SkinThickness, pnorm)
+shapiro.test(diabetes.data$SkinThickness)
+
+
+
+######################################
+## Categorical data analysis (2 x 2)
+######################################
+
+#### 2 x 2 contingency table
+table(diabetes.data$High.Pregnancy, diabetes.data$Outcome)
+
+#### Chi square test -- Method #1
+# H0: The number of high pregnancies in the group with the Outcome is
+#     not different from the group without the Outcome (H0: n1 = n2)
+# Ha: The number of high pregnancies in the group with the Outcome is
+#     different from the group without the Outcome (Ha: n1 != n2)
+chisq.test(diabetes.data$High.Pregnancy, diabetes.data$Outcome)
+
+#### Chi square test -- Method #2
+### install.packages("gmodels") ### Code used to install the gmodels package
+library("gmodels")
+
+CrossTable(diabetes.data$High.Pregnancy, diabetes.data$Outcome, chisq = TRUE, missing.include = TRUE)
+
+
+#### Fisher's exact test
+# When there are cells with < 5 observations or when the total observations
+# is less than 20; these are just general rules. Users should apply these
+# at their discretion.
+fisher.test(diabetes.data$High.Pregnancy, diabetes.data$Outcome)
+
+
+
+
+
